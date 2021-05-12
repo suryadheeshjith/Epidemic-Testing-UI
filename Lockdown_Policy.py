40dd4d8c7d746cb593d61c53d8e1b2246de3d4a0
suryadheeshjith <Surya.dheeshjith@gmail.com>
2021-05-12 14:16:26 +0530

    Tabs to spaes

--- Lockdown_Policy.py
+++ Lockdown_Policy.py
@@ -1,40 +1,40 @@
 import random
 
 class agent_policy_based_lockdown():
-	def __init__(self,policy_to_consider,value_list,do_lockdown_fn,time_period):
-		self.policy_type='Restrict'
-		self.policy_to_consider = policy_to_consider
-		self.do_lockdown_fn=do_lockdown_fn
-		self.value_list=value_list
-		self.time_period = time_period
+    def __init__(self,policy_to_consider,value_list,do_lockdown_fn,time_period):
+        self.policy_type='Restrict'
+        self.policy_to_consider = policy_to_consider
+        self.do_lockdown_fn=do_lockdown_fn
+        self.value_list=value_list
+        self.time_period = time_period
 
-	def enact_policy(self,time_step,agents):
-		if self.do_lockdown_fn(time_step):
-			for agent in agents:
-				history = agent.testing_hist
-				if(len(history)):
-					last_time_step = history[-1].time_step
-					if(time_step - last_time_step <=self.time_period):
-						result = self.get_accumulated_result(history,last_time_step)
-						if(result in self.value_list):
-							if(last_time_step==time_step):
-								if(agent.state!="Infected"):
-									agent.quarantine_list.append("Wrong")
+    def enact_policy(self,time_step,agents):
+        if self.do_lockdown_fn(time_step):
+            for agent in agents:
+                history = agent.testing_hist
+                if(len(history)):
+                    last_time_step = history[-1].time_step
+                    if(time_step - last_time_step <=self.time_period):
+                        result = self.get_accumulated_result(history,last_time_step)
+                        if(result in self.value_list):
+                            if(last_time_step==time_step):
+                                if(agent.state!="Infected"):
+                                    agent.quarantine_list.append("Wrong")
 
-								else:
-									agent.quarantine_list.append("Right")
+                                else:
+                                    agent.quarantine_list.append("Right")
 
-							else:
-								agent.quarantine_list.append(agent.quarantine_list[-1])
-							agent.quarantined= True
+                            else:
+                                agent.quarantine_list.append(agent.quarantine_list[-1])
+                            agent.quarantined= True
 
-	def get_accumulated_result(self,history,last_time_step):
+    def get_accumulated_result(self,history,last_time_step):
 
-		indx = len(history)-1
-		while(indx>=0 and history[indx].time_step==last_time_step):
-			if(history[indx].result == "Negative"):
-				return "Negative"
+        indx = len(history)-1
+        while(indx>=0 and history[indx].time_step==last_time_step):
+            if(history[indx].result == "Negative"):
+                return "Negative"
 
-			indx-=1
+            indx-=1
 
-		return "Positive"
+        return "Positive"
